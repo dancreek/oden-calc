aee3ddf57e75158da1ee61746949642c74a4f725
dancreek <dancreek@gmail.com>
2021-12-06 15:36:25 -0600

    WC: Initial Interface completed

---
+++ calc.js
@@ -0,0 +1,19 @@
+buildInterface();
+
+//Create an interface with buttons and a read-out
+function buildInterface() {
+    const calclator = document.querySelector('.calculator');
+
+    //create a display
+    const display = document.createElement('div')
+    for()
+}
+
+//the interface should start with zero
+//listeners should be setup on all buttons. 
+//when the user taps a button or keyboard button, the display should show that number
+//entries should validate and only allow a decimal once
+//tapping an operator should store and display that selection and highlight the button and store the current number.
+//tapping an operator again should perform the operation with the number on the screen 
+//choosing a new number after an operator should clear teh screen and start the number entry process again.
+//
